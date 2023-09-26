3ae7d7e195184cadcd7b485fafa5b91125252b22
Ronit Sarkar <iamrony777@proton.me>
2023-09-26 12:32:03 +0000

    Tue, Sep 26, 2023, 6:02 PM +05:30

--- src/web/wakatime.ts
+++ src/web/wakatime.ts
@@ -263,7 +263,6 @@ export class WakaTime {
   }
 
   public openDashboardWebsite(): void {
-    this.logger.debug(this.config.keys().toString())
     let url = 'https://wakatime.com/';
     vscode.window.showInformationMessage(this.config.keys().toString())
     vscode.env.openExternal(vscode.Uri.parse(url));
